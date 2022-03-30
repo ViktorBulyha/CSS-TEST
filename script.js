648044a66f4e383fe6d8731932d5874fefe4f35e
Viktor Bulyha <viktor.bulyha@gmail.com>
2022-03-30 19:02:12 +0300

    Add JS code for Lessons

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿//JavaScript Document
+
+//const person = {}
+const person = new Object({
+  name: 'Maximus',
+  age: 25,
+  greet: function () {
+    console.log('Greet!')
+  },
+})
+Object.prototype.sayHello = function () {
+  console.log('Hello!')
+}
+Math.E
